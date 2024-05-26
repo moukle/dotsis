cea1ada9dce867c743c10be47983af4d059aa6a0
Moritz Klein <moritz.klein@pm.me>
2024-05-26 18:25:42 +0200

    Separate packages and logic

---
+++ .scripts/packages.sh
@@ -0,0 +1,64 @@
+options_packages=(
+    "packages_cli"
+    "packages_gui"
+    "packages_wm"
+    "packages_nvidia"
+    "packages_fonts"
+    "packages_gaming"
+    "packages_laptop"
+)
+
+packages_cli=(
+    "curl"
+    "fish"
+    "fzf"
+    "git"
+    "gotop"
+    "neovim"
+    "pywal"
+    "starship"
+    "tmux"
+    "unzip"
+    "yazi"
+    "zsh"
+)
+
+packages_gui=(
+    "code"
+    "firefox"
+    "kitty"
+    "pavucontrol"
+    "zathura"
+)
+
+packages_wm=(
+    "anyrun"
+    "dunst"
+    "hyprland"
+    "libnotify"
+    "pipewire"
+    "waybar"
+)
+
+packages_nvidia=(
+    "nvidia"
+    "nvidia-utils"
+    "cuda"
+)
+
+packages_fonts=(
+    "ttf-maple"
+    "lexend-fonts-git"
+)
+
+packages_gaming=(
+    "ddnet"
+    "steam"
+    "vulkan-icd-loader"
+    "lib32-vulkan-icd-loader"
+)
+
+packages_laptop=(
+    "bluez"
+    "blueman"
+)
--- .scripts/post_install.sh
+++ .scripts/post_install.sh
@@ -1,76 +1,9 @@
 #! /usr/bin/env bash
 
+# quit on fail
 set -e
 
-packages_base=(
-    "gum"
-    "stow"
-)
-
-packages_cli=(
-    "curl"
-    "fish"
-    "fzf"
-    "git"
-    "gotop"
-    "neovim"
-    "pywal"
-    "starship"
-    "tmux"
-    "unzip"
-    "yazi"
-    "zsh"
-)
-
-packages_gui=(
-    "code"
-    "firefox"
-    "kitty"
-    "pavucontrol"
-    "zathura"
-)
-
-packages_wm=(
-    "anyrun"
-    "dunst"
-    "hyprland"
-    "libnotify"
-    "pipewire"
-    "waybar"
-)
-
-packages_nvidia=(
-    "nvidia"
-    "nvidia-utils"
-    "cuda"
-)
-
-packages_fonts=(
-    "ttf-maple"
-    "lexend-fonts-git"
-)
-
-packages_gaming=(
-    "ddnet"
-    "steam"
-    "vulkan-icd-loader"
-    "lib32-vulkan-icd-loader"
-)
-
-packages_laptop=(
-    "bluez"
-    "blueman"
-)
-
-options_packages=(
-    "packages_cli"
-    "packages_gui"
-    "packages_wm"
-    "packages_nvidia"
-    "packages_fonts"
-    "packages_gaming"
-    "packages_laptop"
-)
+source ./packages.sh
 
 install_pkgs() {
     echo " Installing: $@"
@@ -96,9 +29,6 @@ stow_pkgs()
 }
 
 gum_style_list() {
-    # header = $1
-    # content = ${@:2}
-
     gum style \
         --border normal --border-foreground 8 \
         --align center \
@@ -108,18 +38,13 @@ gum_style_list() {
 }
 
 # install required pkgs
+packages_base=( "gum" "stow")
 install_pkgs ${packages_base[@]}
 
 # welcome
 gum style --border normal --margin "1" --padding "1 2" --border-foreground 2 "Hello, there! Welcome to $(gum style --foreground 2 'Post-Installer')."
 
-# pick packages
-
-# gum join \
-#     "$(gum_style_list "base" ${packages_base[@]})" \
-#     "$(gum_style_list "cli" ${packages_cli[@]})" \
-#     "$(gum_style_list "wm" ${packages_wm[@]})"
-
+# print packages
 gum_command="gum join"
 for option in ${options_packages[@]}; do
     group_name=${option#*_}
@@ -128,13 +53,14 @@ for option in ${options_packages[@]}; do
 done
 eval $gum_command
 
-SELECT_PACKAGES=$(gum choose --no-limit \
+# pick packages
+select_options=$(gum choose --no-limit \
     --header "Installing <space>" ${options_packages[@]})
     # --selected=${options_packages[0]},${options_packages[1]} \
-gum log "Selected: " ${SELECT_PACKAGES[@]}
+gum log "Selected: " ${select_options[@]}
 
 # install and stow
-for package_set in ${SELECT_PACKAGES[@]}; do
+for package_set in ${select_options[@]}; do
     echo $package_set
     cd ../${package_set#*_}
     for pkgs in $package_set[@]
