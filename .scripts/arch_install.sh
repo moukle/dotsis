e021d5726c93e2dd5c72808619da1d932fd5ba17
Moritz Klein <moritz.klein@pm.me>
2024-05-26 18:05:45 +0200

    Add yay to arch script

--- .scripts/arch_install.sh
+++ .scripts/arch_install.sh
@@ -1,4 +1,18 @@
 #! /usr/bin/env bash
 
 echo -e -n "\t pacman beepbop"
-sleep 0.2
+exit 0
+
+# install yay
+if ! command -v yay &> /dev/null; then
+    cd /tmp
+
+    pacman -Sy --needed git base-devel
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si
+
+    cd -
+fi
+
+yay -Sy --needed --noconfirm $@
--- .scripts/post_install.sh
+++ .scripts/post_install.sh
@@ -12,31 +12,54 @@ packages_cli=(
     "fish"
     "fzf"
     "git"
-    "git"
     "gotop"
     "neovim"
     "pywal"
     "starship"
     "tmux"
+    "unzip"
     "yazi"
     "zsh"
 )
 
 packages_gui=(
+    "code"
+    "firefox"
     "kitty"
-    "vscode"
+    "pavucontrol"
+    "zathura"
 )
 
 packages_wm=(
     "anyrun"
     "dunst"
     "hyprland"
+    "libnotify"
+    "pipewire"
     "waybar"
 )
 
 packages_nvidia=(
     "nvidia"
+    "nvidia-utils"
     "cuda"
+)
+
+packages_fonts=(
+    "ttf-maple"
+    "lexend-fonts-git"
+)
+
+packages_gaming=(
+    "ddnet"
+    "steam"
+    "vulkan-icd-loader"
+    "lib32-vulkan-icd-loader"
+)
+
+packages_laptop=(
+    "bluez"
+    "blueman"
 )
 
 options_packages=(
@@ -44,13 +67,16 @@ options_packages=(
     "packages_gui"
     "packages_wm"
     "packages_nvidia"
+    "packages_fonts"
+    "packages_gaming"
+    "packages_laptop"
 )
 
 install_pkgs() {
     echo " Installing: $@"
     if ! command -v gum &> /dev/null
     then
-        arch_install $@
+        ./arch_install.sh $@
     else
         gum spin --title "..." --spinner dot --show-output -- ./arch_install.sh $@
     fi
@@ -88,10 +114,19 @@ install_pkgs ${packages_base[@]}
 gum style --border normal --margin "1" --padding "1 2" --border-foreground 2 "Hello, there! Welcome to $(gum style --foreground 2 'Post-Installer')."
 
 # pick packages
-gum join \
-    "$(gum_style_list "base" ${packages_base[@]})" \
-    "$(gum_style_list "cli" ${packages_cli[@]})" \
-    "$(gum_style_list "desktop" ${packages_desktop[@]})"
+
+# gum join \
+#     "$(gum_style_list "base" ${packages_base[@]})" \
+#     "$(gum_style_list "cli" ${packages_cli[@]})" \
+#     "$(gum_style_list "wm" ${packages_wm[@]})"
+
+gum_command="gum join"
+for option in ${options_packages[@]}; do
+    group_name=${option#*_}
+    pkgs=$(eval "echo \${${option}[@]}")
+    gum_command+=" \"\$(gum_style_list \"$group_name\" $pkgs)\""
+done
+eval $gum_command
 
 SELECT_PACKAGES=$(gum choose --no-limit \
     --header "Installing <space>" ${options_packages[@]})
@@ -99,8 +134,8 @@ SELECT_PACKAGES=$(gum choose --no-limit \
 gum log "Selected: " ${SELECT_PACKAGES[@]}
 
 # install and stow
-for package_set in $SELECT_PACKAGES
-do
+for package_set in ${SELECT_PACKAGES[@]}; do
+    echo $package_set
     cd ../${package_set#*_}
     for pkgs in $package_set[@]
     do
@@ -110,3 +145,7 @@ do
     cd -
     echo ""
 done
+
+# TODO: wal
+# ln -sf "${HOME}/.cache/wal/dunstrc"   "${HOME}/.config/dunst/dunstrc"
+# ln -sf "${HOME}/.cache/wal/zathurarc" "${HOME}/.config/zathura/zathurarc"
